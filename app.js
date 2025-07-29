2a0c05cc9d0daf9a18faa1bd60ef628ce2c23c46
Rodrigo Rios <rr415628@gmail.com>
2025-07-29 15:03:30 -0600

    Actualizacion

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-//Crear un array para almacenar los nombres
-let amigos = [];
+function agregarAmigo(){
+    //capturar el valor del campo de entrada
+    let input = document.getElementById('entrada');
+    let nombreAmigo = input.value.trim();
+    alert(nombreAmigo);
+}
 
-//Implementa una funcion para agregar amigos
-document.getElementById
+//validar si el campo esta vacio 
+if(!nombreAmigo){
+    alert('El campo no puede estar vacio');
+    return;
+}
 
-
+//obtener la lista de amigos existente 
+let amigos = JSON.parse(localStorage.getItem)
