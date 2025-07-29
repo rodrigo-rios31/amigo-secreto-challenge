e1f7f0931b745aab09d63e7266485cd7a41442b9
Rodrigo Rios <rr415628@gmail.com>
2025-07-29 16:11:34 -0600

    Actualizacion 4

--- app.js
+++ app.js
@@ -4,4 +4,19 @@ function agregarAmigo(){
     let input = document.getElementById('entrada');
     let nombreAmigo = input.value;
     alert(nombreAmigo);
+
+    if (nombreAmigo === ""){
+    alert(' Por favor escribe el nombre');
+    return;
+}
+hacerLista(nombreAmigo);
+}
+function hacerLista(nombreAmigo){
+//Guardar el nombre en una lista simple
+let listaAmigos = document.getElementById('listaAmigos');
+let nuevoAmigo = document.createElement('li');
+nuevoAmigo.textContent = nombreAmigo;
+listaAmigos.appendChild(nuevoAmigo);
+//limpiar el campo de entrada 
+document.getElementById('entrada').value = "";
 }
